c1d192a3a8ebc568cb3c28574e247edd3602d29a
Samie <samrasolomon40@gmail.com>
2022-08-18 15:24:12 +0000

    Added a file

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,20 @@
+// This script will process
+const kue = require('kue');
+
+const queue = kue.createQueue();
+const blackList = ['4153518780', '4153518781'];
+
+function sendNotification (phoneNumber, message, job, done) {
+  job.progress(0, 100);
+  if (blackList.includes(phoneNumber)) {
+    return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  } else {
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  }
+  done();
+}
+
+queue.process('push_notification_code_2', 2, function (job, done) {
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
